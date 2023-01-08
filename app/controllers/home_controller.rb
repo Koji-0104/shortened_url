28806292dc6a7d81461e0427fe70e4c9fa20b1a0
Koji-0104 <90697671+Koji-0104@users.noreply.github.com>
2023-01-08 17:17:59 +0900

    ActiveRecord start-end no-limit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
   def index
     @current_time = Time.zone.now
-    @news = New.where('start_day <=?', Time.now)
-               .where('end_day >=?', Time.now)
+    @news = New.where('end_day >=?', Time.now)
+               .or(New.where(end_day: nil))
+               .where('start_day <=?', Time.now)
   end
 end
